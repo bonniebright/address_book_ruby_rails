4f2dbc878e34df1d5ae376bc3ba78965a5def9fa
Bonnie Bright & Ben Birdsall <student@epicodus.com>
2014-03-31 12:05:59 -0700

    Initial commit of wikipages contact book.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = '1605e2470712af26527e9151acd6ded480adc89df16c77814e28e10bd61fe0d4944e855d49dea06ee637029d0f9a616323e277c9e1054c0396165b4a9d8fc8a2'
